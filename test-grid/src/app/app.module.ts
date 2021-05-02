7fba824eba9cc1887f0021777e60bd0419c1ae89
Lucas <lucastumia@gmail.com>
2021-05-02 12:46:30 +0200

    detail & sort grid

--- test-grid/src/app/app.module.ts
+++ test-grid/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailGridComponent } from './detail-grid/detail-grid.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import { SortGridComponent } from './sort-grid/sort-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailGridComponent,
+    SortGridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
