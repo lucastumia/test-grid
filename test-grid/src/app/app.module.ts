93e21ceaa7101c146614bd3ec33bdcd69b5851dc
ltumia <lucas.tumia@planningforce.com>
2021-05-12 16:20:18 +0200

    jqx test grid

--- test-grid/src/app/app.module.ts
+++ test-grid/src/app/app.module.ts
@@ -1,29 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DetailGridComponent } from './detail-grid/detail-grid.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DetailGridComponent} from './detail-grid/detail-grid.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+
+import {JqxTestsComponent} from './jqx-tests/jqx-tests.component';
+import {jqxBarGaugeModule} from 'jqwidgets-ng/jqxbargauge';
+import {jqxTreeGridModule} from 'jqwidgets-ng/jqxtreegrid';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DetailGridComponent,
     ToolbarComponent,
+    JqxTestsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
-    MatSortModule
+    MatSortModule,
+    jqxBarGaugeModule,
+    jqxTreeGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
